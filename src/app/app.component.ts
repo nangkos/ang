8815d20f2166069218e56d49b73d9a9a6f8761d8
Nangko <kr.dotware@gmail.com>
2017-12-09 16:18:59 +0900

    init angular2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  userList: Array<User> = [];
+  user: User = new User();
+
+  constructor() {
+  }
+  addUser(): void {
+    this.userList.push(this.user);
+    this.user = new User();
+  }
+  test(): void {
+    let user1: User = new User();
+    user1.userName = '낭만코알라';
+    user1.userAge = 21;
+    this.userList.push(user1);
+    user1 = new User();
+    user1.userName = '낭코아저씨';
+    user1.userAge = 18;
+    this.userList.push(user1);
+  }
 }
